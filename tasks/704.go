276289c3e65373d34c6b01d45cee7166542ae460
ReedRay <ireedray@gmail.com>
2022-09-15 14:45:08 +0300

    704. Binary Search

---
+++ tasks/704.go
@@ -0,0 +1,25 @@
+package main
+
+func search(nums []int, target int) int {
+
+	left, right := 0, len(nums)-1
+	mid := 0
+	for left <= right {
+		mid = (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	x := []int{-1, 0, 3, 5, 9, 12}
+
+	search(x, 9)
+}
