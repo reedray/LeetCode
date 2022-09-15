466ffd2013c9cd356aad5b9e8518ec8b76c21e06
ReedRay <ireedray@gmail.com>
2022-09-15 15:12:20 +0300

    278. First Bad Version

---
+++ tasks/278.go
@@ -0,0 +1,19 @@
+package main
+
+func firstBadVersion(n int) int {
+	left, mid := 0, 0
+	for left < n {
+		mid = left + (n-left)/2
+		if isBadVersion(mid) {
+			n = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
+
+func main() {
+
+}
