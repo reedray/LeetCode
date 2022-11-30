885c59d339594c14632aaea2fd33b9b5ff53383c
ReedRay <ireedray@gmail.com>
2022-11-30 22:37:04 +0300

    1207. Unique Number of Occurrences

---
+++ tasks/1207.go
@@ -0,0 +1,20 @@
+package main
+
+func uniqueOccurrences(arr []int) bool {
+	hm := make(map[int]int)
+	hm2 := make(map[int]int)
+	for _, val := range arr {
+		hm[val]++
+	}
+	for _, val := range hm {
+		if _, ok := hm2[val]; !ok {
+			hm2[val]++
+		} else {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+
+}
